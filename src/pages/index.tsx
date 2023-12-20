717a6af42905c9888c18f810de86a5604841df1b
Håkon Hoelsæter <hakon1108@gmail.com>
2023-12-20 23:50:10 +0100

    Fixed problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,4 @@
-import type {InferGetStaticPropsType } from 'next'
-
-type task = {
-  _id: string
-  name: string
-}
-
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/tasks", {
-    method: "GET",
-  })
-  const testSvar: task[] = await res.json()
-  return { props: { testSvar }}
-}
-
-export default function Home({ testSvar }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   return (
     <h1 className='font-bold text-3xl'>Hei</h1>
   )
