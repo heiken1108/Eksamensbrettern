0cc4bcf68f630bf1209f279ff4588a5c738561db
Håkon Hoelsæter <hakon1108@gmail.com>
2024-01-02 01:01:27 +0100

    Added some operators

--- src/data/operators.tsx
+++ src/data/operators.tsx
@@ -18,8 +18,32 @@ const operators = [
         name: "Division",
         sign: "/",
         calculate: division
+    }, 
+    {
+        name: "Cosine",
+        sign: "cos",
+        calculate: cosine
+    },
+    {
+        name: "Sine",
+        sign: "sin",
+        calculate: sine
+    },
+    {
+        name: "Tangent",
+        sign: "tan",
+        calculate: tan
+    }, 
+    {
+        name: "Sum",
+        sign: "sum",
+        calculate: sum
+    },
+    {
+        name: "Product",
+        sign: "prod",
+        calculate: product
     }
-
 ]
 
 function addition(variables: number[]): number {
@@ -36,6 +60,34 @@ function multiplication(variables: number[]): number {
 
 function division(variables: number[]): number {
     return variables[0] / variables[1];
+}
+
+function cosine(variables: number[]): number {
+    return Math.cos(variables[0]);
+}
+
+function sine(variables: number[]): number {
+    return Math.sin(variables[0]);
+}
+
+function tan(variables: number[]): number {
+    return Math.tan(variables[0]);
+}
+
+function sum(variables: number[]): number {
+    let sum = 0;
+    variables.forEach(variable => {
+        sum += variable;
+    });
+    return sum;
+}
+
+function product(variables: number[]): number {
+    let product = 1;
+    variables.forEach(variable => {
+        product *= variable;
+    });
+    return product;
 }
 
 export default operators;
